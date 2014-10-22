a3701b810896163ac325cc436f15c72f67a1e652
Arvid <arvidspost@gmail.com>
2014-10-22 23:26:32 +0200

    Extended the amount of symbols.

--- src/Symbols.java
+++ src/Symbols.java
@@ -2,19 +2,30 @@ import java.util.TreeSet;
 
 class Symbols {
 
-  private TreeSet<String> symbols;
-  private TreeSet<String> eosSymbols;
+  private final TreeSet<String> symbols;
+  private final TreeSet<String> isSymbols;
+  private final TreeSet<String> eosSymbols;
 
   public Symbols() {
     symbols = new TreeSet<String>();
+    isSymbols = new TreeSet<String>();
     eosSymbols = new TreeSet<String>();
+    
     eosSymbols.add(".");
-    symbols.add(":");
     eosSymbols.add("!");
     eosSymbols.add("?");
-    symbols.add("]");
-    symbols.add(")");
-    symbols.add("\"");
+    
+    isSymbols.add(":");
+    isSymbols.add("]");
+    isSymbols.add(")");
+    isSymbols.add("\"");
+    isSymbols.add("[");
+    isSymbols.add("(");
+    isSymbols.add("\\");
+    isSymbols.add("/");
+    
+    symbols.addAll(eosSymbols);
+    symbols.addAll(isSymbols);
   }
 
   public TreeSet<String> getEOSSymbols() {
@@ -28,8 +39,30 @@ class Symbols {
   public boolean isEosSymbol(String symbol) {
     return eosSymbols.contains(symbol);
   }
+  
+    
+  public boolean wordEndsWithIsSymbol(String word) {
+    if (word.length() > 0) {
+      final String lastChar = word.substring(word.length() - 1, word.length());
+      if (isSymbols.contains(lastChar)) {
+        return true;
+      }
+    }
+    return false;
+    
+  }
 
   public boolean wordEndsWithEOSSymbol(String word) {
+    if (word.length() > 0) {
+      final String lastChar = word.substring(word.length() - 1, word.length());
+      if (eosSymbols.contains(lastChar)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  public boolean wordEndsWithSymbol(String word) {
     if (word.length() > 0) {
       final String lastChar = word.substring(word.length() - 1, word.length());
       if (symbols.contains(lastChar)) {
@@ -38,4 +71,15 @@ class Symbols {
     }
     return false;
   }
+  
+  public boolean wordStartsWithSymbol(String word) {
+    if (word.length() > 0) {
+      final String firstChar = word.substring(0, 1);
+      if (symbols.contains(firstChar)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
 }
