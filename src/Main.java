bc2db5c28f0755f81cdd318573532f316b5de69e
Arvid <arvidspost@gmail.com>
2014-11-02 00:10:24 +0100

    Changed Main, args[0] = filepath , args[1] = number of sentences to train

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 import java.io.File;
 
+/**
+ * @author Arvid
+ *
+ */
 public class Main {
 
+  /**
+   * 
+   * @param args
+   */
   public static void main(String[] args) {
     final String filename = args[0];
+    final int numberOfSentences = Integer.parseInt(args[1]);
     final File file = new File(filename);
-    new SimpleSentenceBoundaryDetection(file,2000);
+    new SentenceBoundaryDetection(file, numberOfSentences);
   }
 }
