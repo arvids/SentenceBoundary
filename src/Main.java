1bc0cd4ac53d05dd5670f7bf0eb60081686d1eb5
Arvid <arvidspost@gmail.com>
2014-11-01 21:22:42 +0100

    Changed main to test on server.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
   public static void main(String[] args) {
     final String filename = args[0];
     final File file = new File(filename);
-    final SentenceBoundary sb = new SentenceBoundary(file);
+    new SimpleSentenceBoundaryDetection(file,20000);
   }
 }
