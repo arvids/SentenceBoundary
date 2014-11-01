15fd9a23e6236ea737280c030ecc6ea6bf33f49f
Arvid <arvidspost@gmail.com>
2014-11-01 21:18:57 +0100

    New sentence2pipe and new evaluation functions. sentence2pipe now produces the n-grams instead of OffsetConjunction

---
+++ src/Sentence2Pipe.java
@@ -0,0 +1,157 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import cc.mallet.pipe.Pipe;
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.Instance;
+import cc.mallet.types.LabelAlphabet;
+import cc.mallet.types.LabelSequence;
+import cc.mallet.types.Token;
+import cc.mallet.types.TokenSequence;
+
+class Sentence2Pipe extends Pipe {
+
+  private static final long    serialVersionUID = 1L;
+  private static final Pattern splitPattern     = Pattern.compile("[^\\s]+");
+  private final Symbols        symbols;
+  private static final int WORD_WINDOW = 5;
+
+  Sentence2Pipe() {
+    super(new Alphabet(), new LabelAlphabet());
+    symbols = new Symbols();
+  }
+
+  @Override
+  public Instance pipe(Instance instance) {
+    
+    final String abstractFileName = (String) instance.getSource();
+    @SuppressWarnings("unchecked")
+    final ArrayList<String> lines = (ArrayList<String>) instance.getData();
+
+    final TokenSequence tokenSequence = new TokenSequence();
+    final LabelSequence labelSequence = new LabelSequence(getTargetAlphabet());
+    // Should probably make a sentence class
+    final ArrayList<Word> wordInfo = new ArrayList<Word>();
+    for (final String line : lines) {
+      if (line.length() == 0) {
+        continue;
+      }
+      final ArrayList<Word> words = getWords(line);
+      if (words.size() == 0) {
+        continue;
+      }
+      // loop through the sentence
+      for (int j = 0; j < words.size(); j++) {
+        String label = SentenceBoundary.IS;
+        if ((j + 1) == words.size()) {
+          label = SentenceBoundary.EOS;
+        }
+        final String currentWord = words.get(j).toString();
+        final String plainCurrentWord = getPlainWord(currentWord);
+        final Token token = new Token(plainCurrentWord);
+        
+        if (symbols.wordEndsWithEOSSymbol(currentWord)) {
+          token.setFeatureValue(getSymbol(currentWord), 1);
+        }
+        //current word as feature
+        token.setFeatureValue(plainCurrentWord + "@0", 1);
+        
+        //unigram for each word before current word.
+        for (int k = 1; j - k > 0 & k < 5; k++) {
+          token.setFeatureValue(words.get(j-k).toString() + "@" + k, 1);
+        }
+        
+        //unigram of each word around current word.
+        for (int l = 1; j + l < words.size() & l < 5; l++) {
+          token.setFeatureValue(words.get(j+l).toString() + "@" + l, 1);
+        }
+        for (int k = 1; j - k > 0 & k < 5; k++) {
+          token.setFeatureValue(words.get(j-k).toString() + "@" + k, 1);
+        }
+        
+        //bigram for each set of words around current word.
+        if (j-2 > 0) {
+          token.setFeatureValue(words.get(j-1).toString() + "_" + words.get(j-2).toString() + "@" + (j-1) + "_" + (j-2), 1);
+        }
+        if (j+2 < words.size()) {
+          token.setFeatureValue(words.get(j+1).toString() + "_" + words.get(j+2).toString() + "@" + (j+1) + "_" + (j+2), 1);
+        }
+        
+        //trigram around word
+        if (j-3 > 0) {
+          token.setFeatureValue(words.get(j-1).toString() + "_" + words.get(j-2).toString() + "_" + words.get(j-3).toString() + "@" + (j-1) + "_" + (j-3), 1);
+        }
+        if (j+3 < words.size()) {
+          token.setFeatureValue(words.get(j+1).toString() + "_" + words.get(j+2).toString() + "_" + words.get(j+3).toString() + "@" + (j+1) + "_" + (j+3), 1);
+        }
+        
+        //position 
+        if (j<3) {
+          token.setFeatureValue("START", 1);
+        } 
+//        else if (j > words.size() - 3) {
+//          token.setFeatureValue("END", 1);
+//        }
+        
+        
+        
+        
+        
+        
+        
+        
+        tokenSequence.add(token);
+        labelSequence.add(label);
+      }
+      wordInfo.addAll(words);
+    }
+    instance.setData(tokenSequence);
+    instance.setTarget(labelSequence);
+    instance.setName(wordInfo);
+    instance.setSource(abstractFileName);
+    return instance;
+  }
+
+  private String getPlainWord(String word) {
+    if (symbols.wordEndsWithSymbol(word)) {
+      word = word.substring(0, word.length() - 1);
+    } else if (symbols.wordStartsWithSymbol(word)) {
+      word = word.substring(1, word.length());
+    }
+    return word;
+  }
+
+  private String getSymbol(String word) {
+    return word.substring(word.length() - 1, word.length());
+  }
+
+  private HashMap<String, Integer> getWordFrequency(ArrayList<String> lines) {
+    final HashMap<String, Integer> freq = new HashMap<String, Integer>();
+    for (int i = 0; i < lines.size(); i++) {
+      final String line = lines.get(i);
+      final ArrayList<Word> words = getWords(line);
+      for (int j = 0; j < words.size(); j++) {
+        final Word word = words.get(j);
+        int count = 0;
+        if (freq.containsKey(word.toString())) {
+          count = freq.get(word.toString());
+        }
+        count++;
+        freq.put(word.toString(), count);
+      }
+    }
+    return freq;
+  }
+
+  private ArrayList<Word> getWords(String line) {
+    final Matcher matcher = splitPattern.matcher(line);
+    final ArrayList<Word> words = new ArrayList<Word>();
+    while (matcher.find()) {
+      final String word = matcher.group();
+      words.add(new Word(word));
+    }
+    return words;
+  }
+}
---
+++ src/SimpleSentenceBoundaryDetection.java
@@ -0,0 +1,404 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import cc.mallet.fst.CRF;
+import cc.mallet.fst.CRFTrainerByLabelLikelihood;
+import cc.mallet.pipe.Pipe;
+import cc.mallet.pipe.SerialPipes;
+import cc.mallet.pipe.TokenSequence2FeatureVectorSequence;
+import cc.mallet.types.Instance;
+import cc.mallet.types.InstanceList;
+import cc.mallet.types.LabelAlphabet;
+import cc.mallet.types.LabelSequence;
+import cc.mallet.types.Sequence;
+
+public class SimpleSentenceBoundaryDetection {
+
+  private int                 numberOfSentences;
+  private static int          numberOfWords;
+  private static final int    numberOfChunks       = 20;
+  private static int          chunkSize;
+  private static File         file;
+  public static final String  EOS                  = "EOS";
+  public static final String  IS                   = "IS";
+  private CRF                 crf;
+  private static ArrayList<String> trainData;
+  private static ArrayList<String> testData;
+  
+
+  public SimpleSentenceBoundaryDetection() {
+    file = new File("FAKE");
+  }
+
+  public SimpleSentenceBoundaryDetection(File file) {
+    this(file, Integer.MAX_VALUE);
+  }
+
+  public SimpleSentenceBoundaryDetection(File file, int numberOfSentences) {
+    this.numberOfSentences = numberOfSentences;
+    SimpleSentenceBoundaryDetection.file = file;
+    numberOfWords = 0;
+    long start = System.currentTimeMillis();
+    setData9010(file, numberOfSentences);
+        
+    this.numberOfSentences = trainData.size();
+    System.out.println("Converted " + numberOfWords + " words to " + this.numberOfSentences
+        + " sentences in " + ((System.currentTimeMillis() - start) / 1000) + " s");
+    chunkSize = this.numberOfSentences / numberOfChunks;
+    System.out.println("ChunkSize: " + chunkSize);
+    start = System.currentTimeMillis();
+    final ArrayList<ArrayList<String>> trainDataChunks = chunks(trainData, chunkSize);
+    System.out.println("Split " + this.numberOfSentences + " in to " + trainDataChunks.size()
+        + " chunks in " + (System.currentTimeMillis() - start) + " ms");
+    final InstanceList instanceList = createTrainningDataFromSentences(trainDataChunks);
+    train(instanceList);
+    writeCRF("crftest");
+    evaluate();
+    
+  }
+
+  public CRF getCRF() {
+    return crf;
+  }
+
+  public void setCRF(CRF crf) {
+    this.crf = crf;
+  }
+
+  public InstanceList createTrainingDataFromSentences(ArrayList<String> sentences) {
+    final LabelAlphabet labelAlphabet = new LabelAlphabet();
+    labelAlphabet.lookupLabel(SimpleSentenceBoundaryDetection.EOS, true);
+    labelAlphabet.lookupLabel(SimpleSentenceBoundaryDetection.IS, true);
+    final Pipe pipe =
+        new SerialPipes(new Pipe[] {new Sentence2Pipe(),
+            new TokenSequence2FeatureVectorSequence(true, true)});
+    final InstanceList instanceList = new InstanceList(pipe);
+    final long start = System.currentTimeMillis();
+    instanceList.addThruPipe(new Instance(sentences, "", "", file.getName()));
+    System.out.println("Completed: " + sentences.size() + "sentences in "
+        + (System.currentTimeMillis() - start) + " ms");
+    return instanceList;
+  }
+
+  public InstanceList createTrainningDataFromSentences(ArrayList<ArrayList<String>> sentencesChunks) {
+    final LabelAlphabet labelAlphabet = new LabelAlphabet();
+    labelAlphabet.lookupLabel(SentenceBoundary.EOS, true);
+    labelAlphabet.lookupLabel(SentenceBoundary.IS, true);
+    final Pipe pipe =
+        new SerialPipes(new Pipe[] {new Sentence2Pipe(),
+            new TokenSequence2FeatureVectorSequence(true, true)});
+    final InstanceList instanceList = new InstanceList(pipe);
+    double chunkPercentage = (chunkSize * 100.0) / numberOfSentences;
+    double total = chunkPercentage;
+    double i = 0.0;
+    final long totalStart = System.currentTimeMillis();
+    for (final ArrayList<String> chunk : sentencesChunks) {
+      final long start = System.currentTimeMillis();
+      instanceList.addThruPipe(new Instance(chunk, "", "", file.getName() + i));
+      i += 1;
+      total = i * chunkPercentage;
+      if (total > 100) {
+        chunkPercentage = 100.0 - ((i - 1) * chunkPercentage);
+        total = 100.0;
+      }
+      System.out.println("Completed: " + chunkPercentage + "% in "
+          + (System.currentTimeMillis() - start) + " ms");
+      System.out.println((total) + "% of Total completed in "
+          + ((System.currentTimeMillis() - totalStart) / 1000) + " s");
+    }
+    return instanceList;
+  }
+  
+  public InstanceList createTestDataFromSentences(ArrayList<String> sentences) {
+    InstanceList testData = new InstanceList(crf.getInputPipe());
+    testData.add(new Instance(sentences, "", "", ""));
+    return testData;
+    
+  }
+
+  public static ArrayList<String> readUkWac(File file) {
+    final ArrayList<String> sentences = new ArrayList<String>();
+    final Symbols symbols = new Symbols();
+    final StringBuilder sb = new StringBuilder();;
+    try {
+      final BufferedReader br = new BufferedReader(new FileReader(file));
+      for (String line; (line = br.readLine()) != null;) {
+        if (line.startsWith("<text")) {
+          continue;
+        } else if (line.equals("<s>")) {
+          sb.setLength(0);
+          continue;
+        } else if (line.equals("</s>")) {
+          sentences.add(sb.toString().toLowerCase());
+          continue;
+        } else {
+          final String s = line.split("\\s")[0];
+          numberOfWords++;
+          if (symbols.isEosSymbol(s)) {
+            sb.append(s);
+          } else {
+            sb.append(" " + s);
+          }
+        }
+      }
+      br.close();
+    } catch (final FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (final IOException e) {
+      e.printStackTrace();
+    }
+    return sentences;
+  }
+
+  public static ArrayList<String> readUkWac(File file, int numberOfSentences) {
+    final ArrayList<String> sentences = new ArrayList<String>();
+    final Symbols symbols = new Symbols();
+    final StringBuilder sb = new StringBuilder();;
+    int i = 0;
+    try {
+      final BufferedReader br = new BufferedReader(new FileReader(file));
+      for (String line; (line = br.readLine()) != null;) {
+        if (i == numberOfSentences) {
+          break;
+        }
+        if (line.startsWith("<text")) {
+          continue;
+        } else if (line.equals("<s>")) {
+          sb.setLength(0);
+          continue;
+        } else if (line.equals("</s>")) {
+          sentences.add(sb.toString());
+          i++;
+          continue;
+        } else {
+          final String s = line.split("\\s")[0];
+          numberOfWords++;
+          if (symbols.isEosSymbol(s)) {
+            sb.append(s);
+          } else {
+            sb.append(" " + s);
+          }
+        }
+      }
+      br.close();
+    } catch (final FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (final IOException e) {
+      e.printStackTrace();
+    }
+    return sentences;
+  }
+  
+  private void setData9010(File file, int numberOfSentences) {
+    trainData = new ArrayList<String>();
+    testData = new ArrayList<String>();
+    Random random = new Random();
+    final Symbols symbols = new Symbols();
+    final StringBuilder sb = new StringBuilder();;
+    int i = 0;
+    try {
+      final BufferedReader br = new BufferedReader(new FileReader(file));
+      for (String line; (line = br.readLine()) != null;) {
+        if (i == numberOfSentences) {
+          break;
+        }
+        if (line.startsWith("<text")) {
+          continue;
+        } else if (line.equals("<s>")) {
+          sb.setLength(0);
+          continue;
+        } else if (line.equals("</s>")) {
+          if (random.nextInt(10) == 9) {
+            testData.add(sb.toString());
+          } else {
+            trainData.add(sb.toString());
+            i++;
+          }
+          continue;
+        } else {
+          final String s = line.split("\\s")[0];
+          numberOfWords++;
+          if (symbols.isEosSymbol(s)) {
+            sb.append(s);
+          } else {
+            sb.append(" " + s);
+          }
+        }
+      }
+      br.close();
+    } catch (final FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (final IOException e) {
+      e.printStackTrace();
+    }
+    System.out.println("Train: " + trainData.size());
+    System.out.println("Test: " + testData.size());
+  }
+
+  public String predict(ArrayList<String> sentences) {
+    final long start = System.currentTimeMillis();
+    final Instance instance = crf.getInputPipe().instanceFrom(new Instance(sentences, "", "", ""));
+    System.out.println("Predict complete");
+    System.out.println("Time: " + (System.currentTimeMillis() - start));
+    return predict(instance);
+  }
+
+  public String predict(Instance instance) {
+    final Sequence input = (Sequence) instance.getData();
+    @SuppressWarnings("unchecked")
+    final ArrayList<Word> words = (ArrayList<Word>) instance.getName();
+    final ArrayList<String> labels = new ArrayList<String>();
+    final Sequence<String> labelresults = crf.transduce(input);
+    for (int i = 0; i < labelresults.size(); i++) {
+      words.get(i).setLabel(labelresults.get(i));
+    }
+    return new Text(words).toString();
+  }
+
+  public void train(InstanceList instanceList) {
+    final long start = System.currentTimeMillis();
+    crf = new CRF(instanceList.getPipe(), (Pipe) null);
+    crf.addStatesForLabelsConnectedAsIn(instanceList);
+    final CRFTrainerByLabelLikelihood crfTrainer = new CRFTrainerByLabelLikelihood(crf);
+    crfTrainer.trainIncremental(instanceList);
+    crf.getInputPipe().getDataAlphabet().stopGrowth();
+    System.out.println("Training complete");
+    System.out.println("Time: " + (System.currentTimeMillis() - start));
+  }
+
+  
+  
+  public void evaluate() {
+    if (crf == null) {
+      new IllegalStateException("CRF not initialized");
+    }
+    final Symbols symbols = new Symbols();
+    int truePositive = 0;
+    int falsePositive = 0;
+    int falseNegative = 0;
+    
+    InstanceList instanceList = createTestDataFromSentences(testData);
+    for (int i = 0; i < instanceList.size(); i++) {
+      Instance instance = instanceList.get(i);
+      String abStractName = (String) instance.getSource();
+      
+      //predict
+      Sequence input = (Sequence) instance.getData();
+      ArrayList<Word> words = (ArrayList<Word>) instance.getName();
+      ArrayList<String> labels = new ArrayList<String>();
+      
+      
+      Sequence<String> output = crf.transduce(input);
+      for (int j = 0; j < output.size(); j++) {
+        labels.add(output.get(j));
+      }
+      
+      for (int k = 0; k < labels.size(); k++) {
+        words.get(k).setLabel(labels.get(k));
+      }    
+      
+      ArrayList<String> orgLabels = labelSequenceToStringArrayList((LabelSequence) instance.getTarget());
+
+      for (int l = 0; l < words.size(); l++) {
+        String word = words.get(l).toString();
+        String prediction = words.get(l).getLabel();
+        String original = orgLabels.get(l);
+
+        if (symbols.wordEndsWithEOSSymbol(word)) { // evaluate only if
+          if (prediction.equals(original))
+            truePositive++;
+          else { // store errors
+            if (prediction.equals("EOS") && original.equals("IS"))
+              falsePositive++;
+            else if (prediction.equals("IS") && original.equals("EOS"))
+              falseNegative++;
+          }
+        }
+      }
+    }
+     
+    double precision = truePositive / (double)(truePositive + falsePositive);
+    double recall = truePositive / (double)(truePositive + falseNegative);
+    double accuracy = truePositive / (double)(truePositive + falsePositive + falseNegative);
+    double f1 = 2 * precision * recall / (precision + recall);
+    
+
+    System.out.println("False positive :" + falsePositive);
+    System.out.println("False negative :" + falseNegative);
+    System.out.println("Recall :" + recall);
+    System.out.println("Precision :" + precision);
+    System.out.println("F1 :" + f1);
+    System.out.println("Accuracy : " + accuracy);
+    
+    
+  }
+  
+  public static ArrayList<ArrayList<String>> chunks(ArrayList<String> sentences, int size) {
+    final ArrayList<ArrayList<String>> chunks = new ArrayList<ArrayList<String>>();
+    for (int i = 0; i < sentences.size(); i += size) {
+      final List<String> chunk = sentences.subList(i, Math.min(sentences.size(), i + size));
+      chunks.add(new ArrayList<String>(chunk));
+    }
+    return chunks;
+  }
+  
+  public ArrayList<String> labelSequenceToStringArrayList(LabelSequence ls) {
+    ArrayList<String> labels = new ArrayList<String>();
+    for (int j = 0; j < ls.size(); j++)
+      labels.add((String) ls.get(j));
+    return labels;
+  }
+
+  public void writeCRF(String filename) {
+    if (crf == null) {
+      new IllegalStateException("CRF not initialized");
+    }
+    try {
+      final FileOutputStream fos = new FileOutputStream(new File(filename + ".gz"));
+      final GZIPOutputStream gout = new GZIPOutputStream(fos);
+      final ObjectOutputStream oos = new ObjectOutputStream(gout);
+      oos.writeObject(crf);
+      oos.close();
+    } catch (final IOException e) {
+      e.printStackTrace();
+      System.exit(0);
+    }
+  }
+
+  public void writeSentences(ArrayList<String> sentences, String filename) {
+    try {
+      final FileWriter writer = new FileWriter(filename);
+      for (final String sentence : sentences) {
+        writer.write(sentence.toString() + "\n");
+      }
+      writer.flush();
+      writer.close();
+    } catch (final IOException e) {
+      e.printStackTrace();
+      System.exit(0);
+    }
+  }
+
+  public void readCRF(String filename) throws IOException, FileNotFoundException,
+      ClassNotFoundException {
+    final FileInputStream fis = new FileInputStream(new File(filename));
+    final GZIPInputStream gin = new GZIPInputStream(fis);
+    final ObjectInputStream ois = new ObjectInputStream(gin);
+    crf = (CRF) ois.readObject();
+    ois.close();
+    crf.getInputPipe().getDataAlphabet().stopGrowth();
+  }
+}
