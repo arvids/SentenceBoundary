565f9552f48f83a70e5ca011f37a1f6f8d8918a5
Arvid <arvidspost@gmail.com>
2014-11-02 12:05:40 +0100

    Added option to change wordwindow.

--- src/Sentence2Pipe.java
+++ src/Sentence2Pipe.java
@@ -11,38 +11,40 @@ import cc.mallet.types.Token;
 import cc.mallet.types.TokenSequence;
 
 /**
+ *
  * @author Arvid
- * 
+ *
  */
 class Sentence2Pipe extends Pipe {
 
   private static final long    serialVersionUID = 1L;
   private static final Pattern splitPattern     = Pattern.compile("[^\\s]+");
   private final Symbols        symbols;
-  private static final int WORD_WINDOW = 5;
-  private final int nGrams;
+  private final int            wordWindow;
+  private final int            nGrams;
 
-  Sentence2Pipe(int nGrams) {
+  Sentence2Pipe(int nGrams, int wordWindow) {
     super(new Alphabet(), new LabelAlphabet());
     symbols = new Symbols();
-    if (nGrams > 0 && nGrams < 4) {
+    if ((nGrams > 0) && (nGrams < 4)) {
       this.nGrams = nGrams;
     } else {
       this.nGrams = 3;
-    } 
+    }
+    if (wordWindow > 0) {
+      this.wordWindow = wordWindow;
+    } else {
+      this.wordWindow = 5;
+    }
   }
-
 
   @Override
   public Instance pipe(Instance instance) {
-    
     final String abstractFileName = (String) instance.getSource();
     @SuppressWarnings("unchecked")
     final ArrayList<String> lines = (ArrayList<String>) instance.getData();
-
     final TokenSequence tokenSequence = new TokenSequence();
     final LabelSequence labelSequence = new LabelSequence(getTargetAlphabet());
-
     final ArrayList<Word> wordInfo = new ArrayList<Word>();
     for (final String line : lines) {
       if (line.length() == 0) {
@@ -52,93 +54,72 @@ class Sentence2Pipe extends Pipe {
       if (words.size() == 0) {
         continue;
       }
-
       for (int j = 0; j < words.size(); j++) {
-        
         /*
-         * Add correct label, it is presumed that the word that is EOS has the string "EOS" attached to it.
-         * 
+         * Add correct label, it is presumed that the word that is EOS has the string "EOS" attached
+         * to it.
          */
         String label = SentenceBoundary.IS;
         String currentWord = words.get(j).toString();
         if (symbols.wordEndsWithEOS(currentWord)) {
           label = SentenceBoundary.EOS;
-          currentWord = currentWord.substring(0, currentWord.length() -3);
+          currentWord = currentWord.substring(0, currentWord.length() - 3);
         }
-        
         final String plainCurrentWord = getPlainWord(currentWord);
         final Token token = new Token(plainCurrentWord);
-        
-        
         /*
-         * Add unigram features, word window is set to 5.
+         * Add uni-gram features, up to wordWindow.
          */
         token.setFeatureValue(plainCurrentWord + "@0", 1);
-        for (int i = 1; j + i < words.size() & i < WORD_WINDOW; i++) {
-          token.setFeatureValue(getPlainWord(words.get(j+i).toString()) + "@" + i, 1);
+        for (int i = 1; ((j + i) < words.size()) & (i < wordWindow); i++) {
+          token.setFeatureValue(getPlainWord(words.get(j + i).toString()) + "@" + i, 1);
         }
-        for (int i = 1; j - i > 0 & i < 5; i++) {
-          token.setFeatureValue(getPlainWord(words.get(j-i).toString()) + "@" + i, 1);
+        for (int i = 1; ((j - i) > 0) & (i < 5); i++) {
+          token.setFeatureValue(getPlainWord(words.get(j - i).toString()) + "@" + i, 1);
         }
+        /*
+         * Add bi-gram features, up to wordWindow
+         */
         if (nGrams > 1) {
           int i = 0;
-          while(j - i - 1 > 0 && i - 1 < WORD_WINDOW){
-            token.setFeatureValue(getPlainWord(words.get(j-i-1).toString()) + "_" + getPlainWord(words.get(j-i).toString()) + "@" + (j-i) + "_" + (j-i-1), 1);
+          while (((j - i - 1) > 0) && ((i - 1) < wordWindow)) {
+            token.setFeatureValue(getPlainWord(words.get(j - i - 1).toString()) + "_"
+                + getPlainWord(words.get(j - i).toString()) + "@" + (j - i) + "_" + (j - i - 1), 1);
             i++;
           }
-          
           i = 0;
-          while(j + i + 1 < WORD_WINDOW && j + i + 1 < words.size()){
-            token.setFeatureValue(getPlainWord(words.get(j+i).toString()) + "_" + getPlainWord(words.get(j+i+1).toString()) + "@" + (j+i) + "_" + (j+i+1), 1);
+          while (((j + i + 1) < wordWindow) && ((j + i + 1) < words.size())) {
+            token.setFeatureValue(
+                getPlainWord(words.get(j + i).toString()) + "_"
+                    + getPlainWord(words.get(j + i + 1).toString()) + "@" + (j + i) + "_"
+                    + (j + i + 1), 1);
             i++;
           }
         }
-        
+        /*
+         * Add tri-gram features, up to wordWindow.
+         */
         if (nGrams > 2) {
           int i = 0;
-          while(j - i - 2 > 0 && i - 2 < WORD_WINDOW){
-            token.setFeatureValue(getPlainWord(words.get(j-i-2).toString()) + "_" + getPlainWord(words.get(j-i-1).toString()) + "_" + getPlainWord(words.get(j-i).toString()) + "@" + (j-i) + "_" + (j-i-2), 1);
+          while (((j - i - 2) > 0) && ((i - 2) < wordWindow)) {
+            token
+                .setFeatureValue(
+                    getPlainWord(words.get(j - i - 2).toString()) + "_"
+                        + getPlainWord(words.get(j - i - 1).toString()) + "_"
+                        + getPlainWord(words.get(j - i).toString()) + "@" + (j - i) + "_"
+                        + (j - i - 2), 1);
             i++;
           }
-          
           i = 0;
-          while(j + i + 2 < WORD_WINDOW && j + i + 2 < words.size()){
-            token.setFeatureValue(getPlainWord(words.get(j+i).toString()) + "_" + getPlainWord(words.get(j+i+1).toString()) + "_" + getPlainWord(words.get(j+i+1).toString()) + "@" + (j+i) + "_" + (j+i+2), 1);
+          while (((j + i + 2) < wordWindow) && ((j + i + 2) < words.size())) {
+            token.setFeatureValue(
+                getPlainWord(words.get(j + i).toString()) + "_"
+                    + getPlainWord(words.get(j + i + 1).toString()) + "_"
+                    + getPlainWord(words.get(j + i + 1).toString()) + "@" + (j + i) + "_"
+                    + (j + i + 2), 1);
             i++;
           }
         }
-        
-//        //bigram for each set of words around current word.
-//        if (j-2 > 0) {
-//          token.setFeatureValue(getPlainWord(words.get(j-1).toString()) + "_" + getPlainWord(words.get(j-2).toString()) + "@" + (j-1) + "_" + (j-2), 1);
-//        }
-//        if (j+2 < words.size()) {
-//          token.setFeatureValue(getPlainWord(words.get(j+1).toString()) + "_" + getPlainWord(words.get(j+2).toString()) + "@" + (j+1) + "_" + (j+2), 1);
-//        }
-        
-//        //trigram around word
-//        if (j-3 > 0) {
-//          token.setFeatureValue(getPlainWord(words.get(j-1).toString()) + "_" + getPlainWord(words.get(j-2).toString()) + "_" + getPlainWord(words.get(j-3).toString()) + "@" + (j-1) + "_" + (j-3), 1);
-//        }
-//        if (j+3 < words.size()) {
-//          token.setFeatureValue(getPlainWord(words.get(j+1).toString()) + "_" + getPlainWord(words.get(j+2).toString()) + "_" + getPlainWord(words.get(j+3).toString()) + "@" + (j+1) + "_" + (j+3), 1);
-//        }
-        
-//        //position 
-//        if (j<3) {
-//          token.setFeatureValue("START", 1);
-//        } 
-//        else if (j > words.size() - 3) {
-//          token.setFeatureValue("END", 1);
-//        }
-        
-        
-        
-        
-        
-        
-        
-        
         tokenSequence.add(token);
         labelSequence.add(label);
       }
